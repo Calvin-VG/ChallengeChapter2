f5cb06f7edf45b23c1be1c188349e5e32dd2940c
unknown <calvin17.cg28@gmail.com>
2022-03-25 20:37:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/challengelayoutcalvinch2/LayoutGojekActivity.kt
@@ -0,0 +1,11 @@
+package com.example.challengelayoutcalvinch2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutGojekActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_gojek)
+    }
+}
---
+++ app/src/main/java/com/example/challengelayoutcalvinch2/LayoutShopeeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.challengelayoutcalvinch2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutShopeeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_shopee)
+    }
+}
